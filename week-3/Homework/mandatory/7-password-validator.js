f783588f2a32dfe0b00c8e07eb5e041a8e23ba63
Alastair Cooper <alastair.cooper@outlook.com>
2020-05-17 17:42:38 +0100

    completed week3-7

--- week-3/Homework/mandatory/7-password-validator.js
+++ week-3/Homework/mandatory/7-password-validator.js
@@ -22,48 +22,72 @@ PasswordValidationResult=  [false, false, false, false, true]
 
 */
 
-function validatePasswords(passwords) {
+const isValidLength = (password) => password.length >= 5;
 
+const hasUpperCase = (password) => password !== password.toLowerCase();
+
+const hasLowerCase = (password) => password !== password.toUpperCase();
+
+const hasNumbers = (password) =>
+  [...Array(10).keys()].filter((number) => password.includes(number)).length !==
+  0;
+
+const hasSymbols = (password) =>
+  ["!", "#", "$", "%", "."].filter((symbol) => password.includes(symbol))
+    .length !== 0;
+
+function validatePasswords(passwords) {
+  const passwordsSeen = [];
+  return passwords.map((password) => {
+    if (passwordsSeen.includes(password)) {
+      return false;
+    } else {
+      passwordsSeen[passwordsSeen.length] = password;
+      return (
+        isValidLength(password) &&
+        hasUpperCase(password) &&
+        hasLowerCase(password) &&
+        hasNumbers(password) &&
+        hasSymbols(password)
+      );
+    }
+  });
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
-const passwords1 = ["Se%5", "TktE.TJTU", "384#HsHF", "dvyyeyy!5", "tryT3729"]
-const passwords2 = ["StUFf27%", "Pl3nty!", "Jai33", "shajsaUA**&&", "Pl3nty!"]
+const passwords1 = ["Se%5", "TktE.TJTU", "384#HsHF", "dvyyeyy!5", "tryT3729"];
+const passwords2 = ["StUFf27%", "Pl3nty!", "Jai33", "shajsaUA**&&", "Pl3nty!"];
 
 function arraysEqual(a, b) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length != b.length) return false;
-  
-    for (let i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
-    }
-  
-    return true;
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+
+  return true;
 }
 
 function test(test_name, expr) {
-    let status;
-    if (expr) {
-      status = "PASSED";
-    } else {
-      status = "FAILED";
-    }
-  
-    console.log(`${test_name}: ${status}`);
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
 }
 
 test(
-   "validatePasswords function works - case 1",
-   arraysEqual(
-      validatePasswords(passwords1), [false, false, true, false, false]
-   )
- );
+  "validatePasswords function works - case 1",
+  arraysEqual(validatePasswords(passwords1), [false, false, true, false, false])
+);
 
- test(
-   "validatePasswords function works - case 2",
-   arraysEqual(
-      validatePasswords(passwords2), [true, true, false, false, false]
-   )
- );
+test(
+  "validatePasswords function works - case 2",
+  arraysEqual(validatePasswords(passwords2), [true, true, false, false, false])
+);
