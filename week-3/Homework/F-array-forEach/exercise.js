46a88be277e8d9904506bb966e69fa4366a792b0
Alastair Cooper <alastair.cooper@outlook.com>
2020-05-20 16:23:08 +0100

    week3-F

--- week-3/Homework/F-array-forEach/exercise.js
+++ week-3/Homework/F-array-forEach/exercise.js
@@ -9,6 +9,14 @@
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+arr.forEach((item) => {
+  let fizzBuzz = "";
+  if (item % 3 === 0) fizzBuzz += "Fizz";
+  if (item % 5 === 0) fizzBuzz += "Buzz";
+  if (fizzBuzz !== "") console.log(fizzBuzz);
+  else console.log(item);
+});
+
 /* EXPECTED OUTPUT */
 
 /*
