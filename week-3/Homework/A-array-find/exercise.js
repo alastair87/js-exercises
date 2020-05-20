ecee311a68244ab7c032e656dea8121d965587b8
Alastair Cooper <alastair.cooper@outlook.com>
2020-05-20 13:18:34 +0100

    week3-A

--- week-3/Homework/A-array-find/exercise.js
+++ week-3/Homework/A-array-find/exercise.js
@@ -5,7 +5,20 @@
 
 // write your code here
 
-var names = ["Rakesh", "Antonio", "Alexandra", "Andronicus", "Annam", "Mikey", "Anastasia", "Karim", "Ahmed"];
+const findLongNameThatStartsWithA = (names) =>
+  names.find((name) => name.startsWith("A") && name.length > 7);
+
+var names = [
+  "Rakesh",
+  "Antonio",
+  "Alexandra",
+  "Andronicus",
+  "Annam",
+  "Mikey",
+  "Anastasia",
+  "Karim",
+  "Ahmed",
+];
 
 var longNameThatStartsWithA = findLongNameThatStartsWithA(names);
 
