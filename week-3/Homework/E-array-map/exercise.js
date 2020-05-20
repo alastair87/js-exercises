3f121ece3fa7323c0825c0a5a2ad8176124c922d
Alastair Cooper <alastair.cooper@outlook.com>
2020-05-20 16:10:58 +0100

    week3-E

--- week-3/Homework/E-array-map/exercise.js
+++ week-3/Homework/E-array-map/exercise.js
@@ -3,3 +3,26 @@
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
+function multiplyBy100(number) {
+  return number * 100;
+}
+
+var numbersBy100 = numbers.map(multiplyBy100);
+
+console.log(numbersBy100);
+
+var numbersBy100 = numbers.map(function multiplyBy100(number) {
+  return number * 100;
+});
+
+console.log(numbersBy100);
+
+var numbersBy100 = numbers.map((number) => {
+  return number * 100;
+});
+
+console.log(numbersBy100);
+
+var numbersBy100 = numbers.map(number => number * 100);
+
+console.log(numbersBy100);
